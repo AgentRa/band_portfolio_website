8670c51642da0da258ca9ee9912cf54321eb026a
agentra <bubonik.san1993@gmail.com>
2024-02-06 18:45:08 +0100

    Feat: implement tickets section

--- src/sections/Tickets/components/TicketRow.tsx
+++ src/sections/Tickets/components/TicketRow.tsx
@@ -8,34 +8,38 @@ type TicketInfoProps = {
 };
 export const TicketRow = ({ date, place, country, ticketLink, facebookLink, mapLink }: TicketInfoProps) => {
   return (
-    <div className="flex h-20 w-full flex-row my-2 place-items-end justify-between rounded-xl p-4 hover:bg-gray-200">
+    <div className="flex h-20 w-full flex-row my-2 duration-500 place-items-end justify-between rounded-xl p-4 hover:bg-gray-200">
       <div className=" flex h-full flex-col justify-between">
         <p className="font-bold">{date}</p>
         <p className="text-gray-500">{place}</p>
       </div>
       <p className="text-gray-500">{country}</p>
       <div>
-        <a href="asdasd">
+        <a href="TODO">
           <button
-            type="button"
-            className="my-1 me-2  rounded-lg  border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+              type="button"
+              className="my-1 me-2  rounded-lg  border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
           >
-              {ticketLink}
+            {ticketLink}
           </button>
         </a>
-        <button
-          type="button"
-          className="my-1 me-2  rounded-lg  border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
-        >
+        <a href="TODO">
+          <button
+              type="button"
+              className="my-1 me-2  rounded-lg  border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+          >
             {facebookLink}
-        </button>
-        <button
-          type="button"
-          className="my-1 me-2  rounded-lg  border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
-        >
+          </button>
+        </a>
+        <a href="TODO">
+          <button
+              type="button"
+              className="my-1 me-2  rounded-lg  border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+          >
             {mapLink}
-        </button>
+          </button>
+        </a>
       </div>
     </div>
-  );
+);
 };
